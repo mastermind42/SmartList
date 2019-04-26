e522e159a76bb408d74714253cd823eae01882bd
Rohit Saxena <rohitmsaxena@gmail.com>
2019-04-25 21:32:53 -0500

    implemented header, delete item, and check item

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  StatusBar,
+  Platform
+} from "react-native";
+import Item from "./components/Item";
+import { Header } from "react-native-elements";
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Header
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          centerComponent={{ text: "Shopping List", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+        />
+        <Item />
       </View>
     );
   }
@@ -14,8 +29,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
